8584ddf963cd810e5c7b541343f69b099a57b141
shiv7297 <2023ucp1655@mnit.ac.in>
2025-03-17 13:47:25 +0000

    variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "shiv@gmail.com"
+var accountPassword = "1111"
+accountCity = "jaipur"
+
+// accountId = 147896
+accountEmail = "nidhi@gmail.com"
+accountPassword = "22222222"
+accountCity = "bengaluru"
+
+console.log(accountId)
+
+console.table([accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";   // Treat all Js code ans Newer Version
+
+// alert("hii")   //we're using NODEJS,not browser
+
+// code redability should be high
+
+console.log("shiv");
+
+let isloggedIn = false
+let name = "nidhi"
+let age = 19
+
+
+// number = 2 to the power 53
+// string = ""
+//boolean = t/f
+//null = standalone value
+//undefined = no value assigned yet
+//symbol = unique
+
+
+// object
+
+console.log(typeof name)
