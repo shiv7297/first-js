47e96d0a67c05d4c54318241c583f0816620c362
shiv7297 <2023ucp1655@mnit.ac.in>
2025-03-19 12:08:30 +0000

    conversion and operation done

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,75 @@
+let score = "sjob"
+
+//console.log(typeof score);
+//console.log( score);
+//console.log(typeof(score))
+
+let value  = Number(score)  // number != Number   case sensitive
+//console.log(typeof value);
+//console.log( value);
+
+
+// "33" = 33
+//"33abc" = NaN
+// true = '1' 
+//"abc" = NaN
+// undefined = NaN  
+
+
+let loggedin = 1
+
+let boolloggedin = Boolean(loggedin)
+//console.log(typeof boolloggedin);
+//console.log(boolloggedin);
+
+
+// 1 = true
+//"" = false
+// "shiv" = true
+
+let num = 45
+
+let strnum = String(num)
+//console.log(strnum)
+//console.log(typeof strnum)
+
+
+//**************************Operations ***************
+
+let val = 3
+let val1 = -val
+//console.log(val1);
+
+//console.log(2**3);  // all operations + - * / power(**) modulo
+
+
+let str1 = "ShivPratap"
+let str2 = " Singh"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+
+//console.log("1" + 2);
+//console.log(1 + "2")//
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+
+//console.log(+true);
+//console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2     //not a good practice
+
+let counter  = 100
+++counter
+console.log(counter);
+
+
+
+
+
+
+
