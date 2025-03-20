dcff5a23024bd938aee370a355242c9baa15981b
shiv7297 <2023ucp1655@mnit.ac.in>
2025-03-20 10:23:10 +0000

    Maths library

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,61 @@
+const score = 400
+
+// console.log(score);
+// console.log(score.length);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.length);
+
+// console.log(score.toString().length);
+// console.log(typeof score);
+
+// console.log(balance.toString().length);
+// console.log(typeof balance);
+
+// console.log(score.toFixed(2));
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toFixed(2));
+
+const hundreds = 10000000000
+// console.log(hundreds.toLocaleString('en-IN'));   //by default is US number system
+
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MIN_VALUE);
+
+// console.log(BigInt.MAX_SAFE_INTEGER);
+// console.log(BigInt.MAX_VALUE);
+// console.log(BigInt.MIN_SAFE_INTEGER);
+// console.log(BigInt.MIN_VALUE);
+
+
+
+// ***************** Maths ***************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+//  round ceil floor min(4,1,2,3,8)  max
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
