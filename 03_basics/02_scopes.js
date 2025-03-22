2943fdff1c64f5f27056d1b2a714240653a7d24c
shiv7297 <2023ucp1655@mnit.ac.in>
2025-03-22 12:15:00 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,56 @@
+// {}
+// not called scope in object because it comes in object definition
+
+
+var c = 400
+d = 500    // problem is with this and var only
+
+if(true){
+    let a = 10
+    const  b = 20
+    var c = 40      // value going out of scope
+    d = 50     // value going out of scope
+}
+
+// console.log(a);
+//console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+// function one () {
+//     const username = "Shiv"
+
+//     function two () {
+//         const website = "Youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+
+//     two()  
+// }
+// one()
+
+if(true){
+    const username = "Shiv"
+    if(true){
+        const website = " resumify"
+        console.log(username + website);
+    }
+    // console.log(website); 
+}
+
+// console.log(username);
+
+//****************** Interesting**************
+  
+console.log(addone(5));   // printing the result even befor initialization
+function addone (num){
+    return num + 1
+}
+
+console.log(addTwo(5));  // can't call the fucntion before initialization because it was declared oin different way
+const addTwo = function(num){
+    return num + 2
+}                                  // also a fucntion but declared in different way
+                                   // also called expression
